a1a96b524923a3d170f9c4a881c5e3f6c4d35917
Joshua Evans <thejoshuaevans@gmail.com>
2020-06-16 17:13:12 -0700

    Webpack Init - Added stubbed webpack functionality

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Trifoia Nodejs Template
-Nodejs template used by Trifoia. Remember to replace this with project specific information
+# BMI Percentile Calculator
+BMI Percentile Calculator
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+};
